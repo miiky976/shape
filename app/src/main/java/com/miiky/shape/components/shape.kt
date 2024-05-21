a36c1b5a1b091c98bff9c8cc8538008287cf1523
miiky976 <miiky976.dev@gmail.com>
2024-05-21 00:01:44 -0600

    ui improvements

--- app/src/main/java/com/miiky/shape/components/shape.kt
+++ app/src/main/java/com/miiky/shape/components/shape.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.gestures.rememberTransformableState
 import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.FitScreen
 import androidx.compose.material.icons.rounded.SearchOff
@@ -13,6 +14,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
@@ -25,8 +27,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.scale
 import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.miiky.shape.R
 import com.miiky.shape.models.Cords
+import kotlin.math.sqrt
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -58,85 +62,93 @@ fun ShapeCanvas(
 		lineColor.value = lineColors[0]
 	}
 
-	Box(
-		modifier = modifier
+	Surface(
+		modifier = modifier,
+		shape = RoundedCornerShape(8.dp),
+		tonalElevation = 4.dp
 	) {
-		val state = rememberTransformableState { scale, pan, _ ->
-			zoom.floatValue = (zoom.floatValue * scale).coerceIn(0.6f, 10f)
-			offset.value += pan / zoom.floatValue
-		}
-		Canvas(
-			modifier = Modifier
-				.fillMaxSize()
-				.transformable(state),
+		Box(
+			modifier = Modifier.fillMaxSize()
 		) {
-			val ws = size.width / xRange.count()  //- (xRange.count()/2 - 1)
-			val xHalf = size.width / 2f
-			val yHalf = size.height / 2f
-			scale(zoom.floatValue) {
-				translate(
-					left = -xRange.elementAt(xRange.count() / 2 - 1) * ws + xHalf + offset.value.x,
-					top = -yRange.elementAt(yRange.count() / 2 - 1) * ws + yHalf + offset.value.y
-				) {
-					// draw the lines
-					xRange.forEach {
-						drawLine(
-							color = lineColor.value,
-							start = Offset(x = it * ws, y = yMin * ws - ws),
-							end = Offset(x = it * ws, y = yMax * ws + 2 * ws)
-						)
-					}
-					yRange.forEach {
-						drawLine(
-							color = lineColor.value,
-							start = Offset(x = xMin * ws - ws, y = it * ws),
-							end = Offset(x = xMax * ws + 2 * ws, y = it * ws)
-						)
-					}
-					// Draw the cords
-					cords.forEach {
+			val state = rememberTransformableState { scale, pan, _ ->
+				zoom.floatValue =
+					(zoom.floatValue * scale).coerceIn(0.6f, sqrt(xRange.count().toFloat()) * 2)
+				offset.value += pan / zoom.floatValue
+			}
+			Canvas(
+				modifier = Modifier
+					.fillMaxSize()
+					.transformable(state),
+			) {
+
+				val ws = if (xRange.count() >= yRange.count()) { size.width / xRange.count() } else { size.height / yRange.count() }
+				val xHalf = size.width / 2f
+				val yHalf = size.height / 2f
+				scale(zoom.floatValue) {
+					translate(
+						left = -xRange.elementAt(xRange.count() / 2 - 1) * ws + xHalf + offset.value.x,
+						top = -yRange.elementAt(yRange.count() / 2 - 1) * ws + yHalf + offset.value.y
+					) {
+						// draw the lines
+						xRange.forEach {
+							drawLine(
+								color = lineColor.value,
+								start = Offset(x = it * ws, y = yMin * ws - ws),
+								end = Offset(x = it * ws, y = yMax * ws + 2 * ws)
+							)
+						}
+						yRange.forEach {
+							drawLine(
+								color = lineColor.value,
+								start = Offset(x = xMin * ws - ws, y = it * ws),
+								end = Offset(x = xMax * ws + 2 * ws, y = it * ws)
+							)
+						}
+						// Draw the cords
+						cords.forEach {
+							drawRect(
+								color = outline,
+								size = Size(ws, ws),
+								topLeft = Offset(
+									x = it.x * ws,
+									y = it.y * ws
+								)
+							)
+						}
+						// Draw Selected
 						drawRect(
-							color = outline,
+							color = primary,
 							size = Size(ws, ws),
 							topLeft = Offset(
-								x = it.x * ws,
-								y = it.y * ws
+								x = selected.x * ws,
+								y = selected.y * ws
 							)
 						)
 					}
-					// Draw Selected
-					drawRect(
-						color = primary,
-						size = Size(ws, ws),
-						topLeft = Offset(
-							x = selected.x * ws,
-							y = selected.y * ws
-						)
-					)
 				}
 			}
-		}
-		IconButton(
-			onClick = {
-				zoom.floatValue = 1f
-			},
-			modifier = Modifier.align(Alignment.BottomEnd)
-		) {
-			Icon(
-				Icons.Rounded.SearchOff,
-				contentDescription = stringResource(id = R.string.resetZoom)
-			)
-		}
-		IconButton(
-			onClick = {
-				offset.value = Offset.Zero
-			},
-			modifier = Modifier.align(Alignment.BottomStart)
-		) {
-			Icon(
-				Icons.Rounded.FitScreen,
-				contentDescription = stringResource(id = R.string.fitScreen)
-			)
+			IconButton(
+				onClick = {
+					zoom.floatValue = 1f
+				},
+				modifier = Modifier.align(Alignment.BottomEnd)
+			) {
+				Icon(
+					Icons.Rounded.SearchOff,
+					contentDescription = stringResource(id = R.string.resetZoom)
+				)
+			}
+			IconButton(
+				onClick = {
+					offset.value = Offset.Zero
+				},
+				modifier = Modifier.align(Alignment.BottomStart)
+			) {
+				Icon(
+					Icons.Rounded.FitScreen,
+					contentDescription = stringResource(id = R.string.fitScreen)
+				)
+			}
 		}
 	}
 }
--- app/src/main/java/com/miiky/shape/screens/LineCords.kt
+++ app/src/main/java/com/miiky/shape/screens/LineCords.kt
@@ -1,24 +1,21 @@
 package com.miiky.shape.screens
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowDropDown
+import androidx.compose.material.icons.rounded.ArrowDropUp
 import androidx.compose.material.icons.rounded.ArrowLeft
 import androidx.compose.material.icons.rounded.ArrowRight
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableIntStateOf
@@ -36,10 +33,13 @@ import com.miiky.shape.models.Cords
 
 @Composable
 fun LineCords(modifier: Modifier = Modifier) {
-	val startX = remember { mutableIntStateOf(-5) }
+	val hidden = remember { mutableStateOf(true) }
+
+	// Cords stuff
+	val startX = remember { mutableIntStateOf(-10) }
 	val startY = remember { mutableIntStateOf(-10) }
-	val endX = remember { mutableIntStateOf(8) }
-	val endY = remember { mutableIntStateOf(-2) }
+	val endX = remember { mutableIntStateOf(20) }
+	val endY = remember { mutableIntStateOf(-9) }
 	val start = remember {
 		mutableStateOf(Cords(startX.intValue, startY.intValue))
 	}
@@ -49,8 +49,10 @@ fun LineCords(modifier: Modifier = Modifier) {
 	var list = remember {
 		Line(start.value, end.value)
 	}
-	val hidden = remember { mutableStateOf(true) }
 	val selected = remember { mutableIntStateOf(0) }
+	fun update() {
+		list = Line(start.value, end.value)
+	}
 	Column(
 		modifier = modifier
 			.fillMaxSize()
@@ -65,16 +67,40 @@ fun LineCords(modifier: Modifier = Modifier) {
 			NumberTextInput(number = endY, label = "end Y", modifier = Modifier.weight(1f))
 		}
 		Row {
-			Switch(checked = hidden.value, onCheckedChange = { hidden.value = it })
 			Button(onClick = {
-				list = Line(start.value, end.value)
+				update()
 			}) {
 				Text(text = "Force update")
 			}
 		}
 		ElevatedCard(
-			modifier = if (hidden.value) {Modifier.weight(1f)}else {Modifier.fillMaxWidth()}
+			modifier = if (hidden.value) {
+				Modifier.weight(1f)
+			} else {
+				Modifier.fillMaxWidth()
+			},
 		) {
+			Row(
+				modifier = Modifier
+					.fillMaxWidth()
+					.clickable {
+						hidden.value = !hidden.value
+					}
+			) {
+				Text(
+					text = "blocks: " + list.size + " ; " + list[selected.intValue].toString(),
+					modifier = Modifier
+						.align(Alignment.CenterVertically)
+						.padding(24.dp)
+						.weight(1f)
+				)
+				Icon(
+					if (!hidden.value) Icons.Rounded.ArrowDropDown else Icons.Rounded.ArrowDropUp, contentDescription = null,
+					modifier = Modifier
+						.align(Alignment.CenterVertically)
+						.padding(24.dp)
+				)
+			}
 			if (hidden.value) {
 				ShapeCanvas(
 					modifier = Modifier.weight(1f),
@@ -82,39 +108,39 @@ fun LineCords(modifier: Modifier = Modifier) {
 					selected = list[selected.intValue]
 				)
 			}
-			Text(text = list[selected.intValue].toString(), modifier = Modifier.padding(24.dp).align(Alignment.CenterHorizontally))
-		}
-		Row(
-			modifier = Modifier.align(Alignment.End)
-		) {
-			FilledIconButton(onClick = {
-				selected.intValue -= 1
-				if (selected.intValue < 0) {
-					selected.intValue = 0
+			Row(
+				modifier = Modifier.padding(10.dp)
+			) {
+				FilledIconButton(onClick = {
+					selected.intValue -= 1
+					if (selected.intValue < 0) {
+						selected.intValue = 0
+					}
+				}) {
+					Icon(
+						Icons.Rounded.ArrowLeft,
+						contentDescription = stringResource(id = R.string.prevItem)
+					)
 				}
-			}) {
-				Icon(
-					Icons.Rounded.ArrowLeft,
-					contentDescription = stringResource(id = R.string.prevItem)
+				Slider(
+					value = selected.intValue.toFloat(),
+					onValueChange = { selected.intValue = it.toInt() },
+					valueRange = (0f..(list.size - 1).toFloat()),
+					modifier = Modifier.weight(1f)
 				)
-			}
-			Slider(
-				value = selected.intValue.toFloat(),
-				onValueChange = { selected.intValue = it.toInt() },
-				valueRange = (0f..(list.size - 1).toFloat()),
-				modifier = Modifier.weight(1f)
-			)
-			FilledIconButton(onClick = {
-				selected.intValue += 1
-				if (selected.intValue > list.size - 1) {
-					selected.intValue = list.size - 1
+				FilledIconButton(onClick = {
+					selected.intValue += 1
+					if (selected.intValue > list.size - 1) {
+						selected.intValue = list.size - 1
+					}
+				}) {
+					Icon(
+						Icons.Rounded.ArrowRight,
+						contentDescription = stringResource(id = R.string.nextItem)
+					)
 				}
-			}) {
-				Icon(
-					Icons.Rounded.ArrowRight,
-					contentDescription = stringResource(id = R.string.nextItem)
-				)
 			}
 		}
 	}
+
 }
